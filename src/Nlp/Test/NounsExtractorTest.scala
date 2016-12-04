fc6cda34381ea2bad628d085a9cb742ef788d2fe
Vitalik <vitaliy.oleshkevich@gmail.com>
2016-12-04 19:59:24 +0100

    Dodanie modułu do testów jednostkowych

---
+++ src/Nlp/Test/NounsExtractorTest.scala
@@ -0,0 +1,23 @@
+package Nlp.Test
+
+import Nlp.NounsExtractor
+import org.scalatest.FunSuite
+
+/**
+  * Created by Vitalik on 2016-12-04.
+  */
+class NounsExtractorTest extends FunSuite {
+
+  test("testNouns1") {
+    val nouns: List[String] = new NounsExtractor("I want to buy a cap").extractNouns
+    assert(nouns(0) == "cap")
+    assert(nouns.length == 1)
+
+  }
+  test("testNouns2"){
+    val nouns : List[String] = new NounsExtractor("Please show me some telephones").extractNouns
+    assert(nouns(0)=="telephones")
+    assert(nouns.length == 1)
+  }
+
+}
