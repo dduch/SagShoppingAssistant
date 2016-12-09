73df5c76987ba52eb613cf653fed5ce2625d6236
Vitalik <vitaliy.oleshkevich@gmail.com>
2016-12-09 11:21:02 +0100

    Dodano prosty analizator częstości wystąpienia nazwy produktu na stronie

--- src/Nlp/PageAnalyzer.scala
+++ src/Nlp/PageAnalyzer.scala
@@ -1,13 +1,35 @@
 package Nlp
 
+import java.io.FileInputStream
+
+import Core.Web.WebDownloader
+import opennlp.tools.tokenize.{TokenizerME, TokenizerModel}
+
 /**
   * Created by Vitalik on 2016-12-07.
   */
 class PageAnalyzer {
   def isPageRelevant(pageUrl: String, query: String): Int = {
-    // TODO analyze webpage, is it relevant to query
-    //println("Analyzing "+pageUrl)
 
-    scala.util.Random.nextInt(100)
+    // Create tokenizer model
+    val english = new FileInputStream("en-token.bin")
+    val model = new TokenizerModel(english)
+    val tokenizer = new TokenizerME(model)
+
+    // Get webpage and tokenize it
+    val doc = new WebDownloader().get(pageUrl)
+    var tokens  = Array[String]()
+    tokens = tokenizer.tokenize(doc.toString())
+
+    // Count the number of occurences of query at the webpage
+    var score : Int = 0
+    for(token <- tokens){
+      if(token.equalsIgnoreCase(query)){
+        score += 1
+      }
+    }
+
+    // Return the number of occurences as a page relevancy score
+    score
   }
 }
